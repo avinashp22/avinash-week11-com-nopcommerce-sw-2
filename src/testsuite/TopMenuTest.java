611f0d88a400ba70456583f82488b6f64b0bf764
avinashp22 <avinashp22@gmail.com>
2023-11-18 20:23:27 +0000

    avinash-week11-com-nopcommerce-sw-2

--- src/testsuite/TopMenuTest.java
+++ src/testsuite/TopMenuTest.java
@@ -1,4 +1,33 @@
 package testsuite;
 
-public class TopMenuTest {
+import browserfactory.BaseTest;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TopMenuTest extends BaseTest {
+
+    String baseURL = "https://demo.nopcommerce.com/"; // set base url
+
+    @Before
+    public void setUP(){openBrowser(baseURL);}
+
+    @Test
+    public void nameHere1(){
+
+
+    }
+
+
+
+    @Test
+    public void nameHere2(){
+
+
+
+    }
+
+    @After
+    public void tearDown(){closeBrowser();}
+
 }
